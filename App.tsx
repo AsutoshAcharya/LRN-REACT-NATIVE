70a78b71e94bbbdfd785328ae234e538030aa4b9
Asutosh Acharya <asutosh246@gmail.com>
2025-08-26 14:39:59 +0530

    react-navigation

--- App.tsx
+++ App.tsx
@@ -1,13 +1,22 @@
 import { Fragment } from "react";
 import { StatusBar } from "expo-status-bar";
+import { NavigationContainer } from "@react-navigation/native";
+
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
 
 import CategoryScreen from "./screens/CategoriesScreen/CategoryScreen";
+
+const Stack = createNativeStackNavigator();
 
 export default function App() {
   return (
     <Fragment>
       <StatusBar style="light" />
-      <CategoryScreen />
+      <NavigationContainer>
+        <Stack.Navigator>
+          <Stack.Screen name="Categories" component={CategoryScreen} />
+        </Stack.Navigator>
+      </NavigationContainer>
     </Fragment>
   );
 }
