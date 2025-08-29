3f529dff3cbd29a7e0f70466a93458e3c8a4adbd
Asutosh Acharya <asutosh246@gmail.com>
2025-08-29 20:36:51 +0530

    minor changes

--- App.tsx
+++ App.tsx
@@ -4,16 +4,17 @@ import { NavigationContainer } from "@react-navigation/native";
 
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
 
-import { routesMap } from "./RoutesMap";
+import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
+import { routesMap, RouteTitle } from "./RoutesMap";
 import FavouriteContextProvider from "./context/favourites/FavouriteContext";
 
 const Stack = createNativeStackNavigator();
-
+const Tab = createBottomTabNavigator();
 export default function App() {
   return (
     <Fragment>
       <StatusBar style="light" />
-      <FavouriteContextProvider>
+      {/* <FavouriteContextProvider> 
         <NavigationContainer>
           <Stack.Navigator
             screenOptions={{
@@ -35,8 +36,30 @@ export default function App() {
               />
             ))}
           </Stack.Navigator>
-        </NavigationContainer>
-      </FavouriteContextProvider>
+</NavigationContainer>
+  
+      </FavouriteContextProvider> */}
+
+      {/* Bottom Tab */}
+      <NavigationContainer>
+        <Tab.Navigator
+          initialRouteName={RouteTitle.AllExpenses}
+          screenOptions={{
+            headerStyle: { backgroundColor: "#0e3b71ff" },
+            headerTitleAlign: "center",
+            headerTintColor: "#FFF",
+          }}
+        >
+          {routesMap.map((route) => (
+            <Tab.Screen
+              name={route.title}
+              component={route.component}
+              key={route.title}
+              {...(route.options && route.options)}
+            />
+          ))}
+        </Tab.Navigator>
+      </NavigationContainer>
     </Fragment>
   );
 }
--- RoutesMap.ts
+++ RoutesMap.ts
@@ -1,13 +1,17 @@
-import { ReactNode } from "react";
+import { createElement, ReactNode } from "react";
+import { Ionicons } from "@expo/vector-icons";
 import CategoryScreen from "./screens/CategoriesScreen/CategoryScreen";
 import MealsOverview from "./screens/MealsOverview/MealsOverview";
 import MealDetails from "./screens/MealDetails/MealDetails";
 import { NativeStackNavigationOptions } from "@react-navigation/native-stack";
+import AllExpenses from "./screens/AllExpenses";
+import { BottomTabNavigationOptions } from "@react-navigation/bottom-tabs";
 
 export enum RouteTitle {
   MealsCategory = "Meals Category",
   MealsOverview = "Meals Overview",
   MealDetails = "Meal Details",
+  AllExpenses = "All Expenses",
 }
 
 export type RootStackParamList = {
@@ -19,19 +23,26 @@ export type RootStackParamList = {
 export type Route = {
   title: RouteTitle;
   component: React.ComponentType<any>;
-  options?: NativeStackNavigationOptions;
+  options?: NativeStackNavigationOptions | BottomTabNavigationOptions;
 };
 export const routesMap: Array<Route> = [
+  // {
+  //   title: RouteTitle.MealsCategory,
+  //   component: CategoryScreen,
+  // },
+  // {
+  //   title: RouteTitle.MealsOverview,
+  //   component: MealsOverview,
+  // },
+  // {
+  //   title: RouteTitle.MealDetails,
+  //   component: MealDetails,
+  // },
   {
-    title: RouteTitle.MealsCategory,
-    component: CategoryScreen,
-  },
-  {
-    title: RouteTitle.MealsOverview,
-    component: MealsOverview,
-  },
-  {
-    title: RouteTitle.MealDetails,
-    component: MealDetails,
+    title: RouteTitle.AllExpenses,
+    component: AllExpenses,
+    // options: {
+    //   tabBarIcon: (props) => createElement(Ionicons, { name: "wallet" }),
+    // },
   },
 ];
