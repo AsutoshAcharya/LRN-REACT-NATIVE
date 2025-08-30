13be6889d171623a89c3b1dbf21a1d3be7b0a63d
Asutosh Acharya <asutosh246@gmail.com>
2025-08-30 20:49:55 +0530

    permission access changes for ios

--- screens/AllExpenses/ImagePicker.tsx
+++ screens/AllExpenses/ImagePicker.tsx
@@ -2,13 +2,16 @@ import {
   launchCameraAsync,
   useCameraPermissions,
   PermissionStatus,
+  ImagePickerResult,
 } from "expo-image-picker";
-import React from "react";
-import { Alert, Button, View } from "react-native";
+import React, { useState } from "react";
+import { Alert, Button, Image, StyleSheet, Text, View } from "react-native";
 
 const ImagePicker = () => {
   const [cameraPermissionIInfo, requestPermission] = useCameraPermissions();
-
+  const [pickedImage, setPickedImage] = useState<ImagePickerResult | null>(
+    null
+  );
   async function verifyPermission() {
     if (cameraPermissionIInfo?.status === PermissionStatus.UNDETERMINED) {
       const permissionResp = await requestPermission();
@@ -28,13 +31,44 @@ const ImagePicker = () => {
       aspect: [16, 9],
       quality: 0.5,
     });
-    console.log(image);
+    setPickedImage(image);
   }
+  console.log(pickedImage);
+
   return (
-    <View>
+    <View style={styles.container}>
+      <View style={styles.imagePreview}>
+        {pickedImage?.assets?.at(0)?.uri ? (
+          <Image
+            style={styles.image}
+            source={{ uri: pickedImage?.assets?.at(0)?.uri }}
+          />
+        ) : (
+          <Text children="No image taken" />
+        )}
+      </View>
       <Button title="Take Image" onPress={takeImage} />
     </View>
   );
 };
+
+const styles = StyleSheet.create({
+  container: {
+    padding: 8,
+  },
+  imagePreview: {
+    width: "100%",
+    height: 200,
+    marginVertical: 8,
+    justifyContent: "center",
+    alignItems: "center",
+    backgroundColor: "cyan",
+    borderRadius: 4,
+  },
+  image: {
+    width: "100%",
+    height: "100%",
+  },
+});
 
 export default ImagePicker;
